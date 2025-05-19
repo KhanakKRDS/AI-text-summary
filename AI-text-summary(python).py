342d792277cedc7aeaa9e1912120763e766168ba
KhanakKRDS <2652104@sd61learn.ca>
2025-05-18 19:29:45 -0700

    completed the code
    need to make it work

--- AI-text-summary(python).py
+++ AI-text-summary(python).py
@@ -1,6 +1,7 @@
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize #used for analyzing words and sentences.
+from collections import Counter
 
 nltk.download("punkt") #used to split the text into sentences
 nltk.download("stopwords") # used to remove words like the, and, is
@@ -19,3 +20,35 @@ sentences, words = understand_text(text)
 
 print("sentences:", sentences)
 print("changes words:", words)
+
+def important_words(words):
+    word_counts = Counter([word for sentence in words for word in sentence])# count how many times the word occurred
+    
+    return word_counts
+
+word_freq = important_words(words)
+print("Word Frequency:", word_freq)
+
+def get_sentence_scores(sentences, word_freq):
+    sentence_scores = {}
+
+    for sentence in sentences:
+        words = word_tokenize(sentence.lower())
+        sentence_scores[sentence] = sum(word_freq[word] for word in words if word in word_freq)
+
+    return sentence_scores
+
+sentence_scores = get_sentence_scores(sentences, word_freq)
+print("sentence scores:", sentence_scores)
+
+
+def get_summary(sentence_scores, top_n=2):
+    sorted_sentences = sorted(sentence_scores.items(), key=lambda x: x[1], reverse = True)
+    summary_sentences = [sentence for sentence, _in sorted_sentences[:top_n]]
+    return"". join(summary_sentences)
+
+summary = get_summary(sentence_scores)
+print("Summary:", summary)
+
+
+
